a1ffb14fc003e0076c671f95b23d8ba684a6d607
okochi-mika <hiyoko_m@icloud.com>
2025-04-05 16:49:05 +0900

    ファイルを追加

--- js/class-operation.js
+++ js/class-operation.js
@@ -1,7 +1,19 @@
 $(function() {
-  // id属性がaddの要素がクリックされたら
-  $('#add').on('click', function(){
-    // id属性がtargetの要素にclass属性'sample'を追加
-    $('#target').addClass('sample');
+  // id属性がtoggleの要素がクリックされたら
+  $('#toggle').on('click', function(){
+    // id属性がtargetの要素にclass属性'sample'を追加／削除する
+    $('#target').toggleClass('sample');
   });
- });
+  // id属性がhasの要素がクリックされたら
+  $('#has').on('click', function(){
+    // id属性がtargetの要素にclass属性'sample'があるか取得する
+    let result = $('#target').hasClass('sample');
+
+    // 条件分岐してconsole.logを表示する
+    if(result) {
+      console.log('sampleクラスがあります');
+    } else {
+      console.log('sampleクラスがありません');
+    }
+  });
+})
